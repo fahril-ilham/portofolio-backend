0e642d1aa7f2d9d606c3b4120b7063ba1c0bf6f9
ZoeyBoy <famsilham99@gmail.com>
2026-01-28 10:39:09 +0700

    update: edit route

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,5 @@
 use App\Http\Controllers\PortofolioController;
 use Illuminate\Support\Facades\Route;
 
-/** This code makes it go from "routes" directly to "view" (not recommended)
-Route::get('/', function () {
-    // return view('welcome');
-    return view('portofolio');
-});
-*/
-
 // This code makes it go from "routes" to "controllers" (recommended)
 Route::get('/', [PortofolioController::class, 'index']);
