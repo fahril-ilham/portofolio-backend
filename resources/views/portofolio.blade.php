fb6d9cfc7e8537a9ccc19f97c6d92fb79c78a337
ZoeyBoy <famsilham99@gmail.com>
2026-01-28 10:39:50 +0700

    update: edit view

--- resources/views/portofolio.blade.php
+++ resources/views/portofolio.blade.php
@@ -10,12 +10,18 @@
     <h1>Fahril Ilham Pangestu</h1>
     <p>Software Engineering Learner</p>
 
-    <h2>Projects</h2>
-    @foreach ($projects as $project)
-        <div>
-            <h3>{{ $project->title }}</h3>
-            <p>{{ $project->description }}</p>
-        </div>
-    @endforeach
+    <h2>My Projects</h2>
+    @if ($projects->count())
+        <ul>
+            @foreach ($projects as $project)
+                <div>
+                    <h3>{{ $project->title }}</h3>
+                    <p>{{ $project->description }}</p>
+                </div>
+            @endforeach
+        </ul>
+    @else
+        <p>Belum ada project.</p>
+    @endif
 </body>
 </html>
