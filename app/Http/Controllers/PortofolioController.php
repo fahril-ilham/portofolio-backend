18f39c03af5c073cdbecd3473721ffb6f2be3d47
ZoeyBoy <famsilham99@gmail.com>
2026-02-02 12:41:06 +0700

    update: adding a create feature

--- app/Http/Controllers/PortofolioController.php
+++ app/Http/Controllers/PortofolioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use Symfony\Component\HttpFoundation\Request;
 
 class PortofolioController extends Controller
 {
@@ -10,5 +11,24 @@ class PortofolioController extends Controller
     {
         $projects = Project::all();
         return view('portofolio', compact('projects'));
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|min:3',
+            'description' => 'required',
+            'tech_stack' => 'nullable'
+        ]);
+
+        // Eloquent ORM, Mass Assignment
+        Project::create($validated);
+
+        return redirect('/')->with('success', 'Project berhasil ditambahkan.');
     }
 }
