bcb2503d5a8d495c5969cdaca9bf439ca756bd88
ZoeyBoy <famsilham99@gmail.com>
2026-02-02 12:38:55 +0700

    update: adding route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 // This code makes it go from "routes" to "controllers" (recommended)
 Route::get('/', [PortofolioController::class, 'index']);
+Route::get('/projects/create', [PortofolioController::class, 'create']);
+Route::post('/projects', [PortofolioController::class, 'store']);
