d531c576fdb805d9bc188270211ffa9ca3fc849c
ZoeyBoy <famsilham99@gmail.com>
2026-02-19 15:37:32 +0700

    update: route API

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 // This code to test API Resource + JSON Response
-Route::get('/projects', [ProjectController::class, 'index']);
+// Route::get('/projects', [ProjectController::class, 'index']);
+
+// This code to access API resource
+Route::apiResource('projects', ProjectController::class);
