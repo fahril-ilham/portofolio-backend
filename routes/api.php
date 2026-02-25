0f09a780ecc4dfb76b1e69e9e1ab3fcb2f643146
ZoeyBoy <famsilham99@gmail.com>
2026-02-25 17:00:06 +0700

    update: edit routes/api

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Route;
 // This code to test API Resource + JSON Response
 // Route::get('/projects', [ProjectController::class, 'index']);
 
-// This code to access API resource
-Route::apiResource('projects', ProjectController::class);
+// This code to access API resource without login
+// Route::apiResource('projects', ProjectController::class);
 
-// This code to access register, login, and logout
+// This code to access API resource through the register, login, and logout features
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
