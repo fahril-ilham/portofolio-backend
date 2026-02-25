8a9783e3b5a3ad270503b329f1713168906e221e
ZoeyBoy <famsilham99@gmail.com>
2026-02-25 14:34:00 +0700

    add: login feature

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+    /**
+     * @method \Laravel\Sanctum\NewAccessToken createToken(string $name, array $abilities = ['*'])
+     */
+
     public function register(Request $request)
     {
         $validated = $request->validate([
@@ -28,5 +34,29 @@ class AuthController extends Controller
             'message' => 'Admin berhasil dibuat.',
             'data' => $user
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages([
+                'email' => ['Email atau password salah.']
+            ]);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('admin-token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login berhasil.',
+            'token' => $token
+        ], 200);
     }
 }
