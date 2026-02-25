2412fe868fdf3d32ca18fc375311ee34bf318b6a
ZoeyBoy <famsilham99@gmail.com>
2026-02-25 16:22:24 +0700

    update: edit AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,15 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    /**
-     * @method \Laravel\Sanctum\NewAccessToken createToken(string $name, array $abilities = ['*'])
-     */
-
     public function register(Request $request)
     {
         $validated = $request->validate([
@@ -26,7 +21,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $validated['name'],
             'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
+            'password' => $validated['password'],
         ]);
 
         return response()->json([
