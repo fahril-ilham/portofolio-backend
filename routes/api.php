057ac1c0205987a78a7fba0d04ed37d0d8741a3c
ZoeyBoy <famsilham99@gmail.com>
2026-04-01 23:19:22 +0700

    update: add API versioning

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,18 @@ use Illuminate\Support\Facades\Route;
 // This code to access API resource without login
 // Route::apiResource('projects', ProjectController::class);
 
-// This code to access API resource through the register, login, and logout features
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+    // This code to access API resource through the register, login, and logout features
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
 
-Route::middleware(['auth:sanctum', 'admin', 'throttle:60,1'])->group(function () {
-    Route::apiResource('projects', ProjectController::class);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
+    Route::middleware(['auth:sanctum', 'admin', 'throttle:60,1'])->group(function () {
+        Route::apiResource('projects', ProjectController::class);
+    });
+
 });
